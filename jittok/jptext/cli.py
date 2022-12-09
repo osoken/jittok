92d7b4ad9038bdf9255ec68f95cea4da5abb174e
osoken <osoken.devel@outlook.jp>
2022-12-09 23:45:52 +0900

    feat(jptext): add main to cli.py

--- jittok/jptext/cli.py
+++ jittok/jptext/cli.py
@@ -1,5 +1,9 @@
-from argparse import ArgumentParser, _SubParsersAction
+from argparse import ArgumentParser, Namespace, _SubParsersAction
 
 
 def setup_argument_subparser(subparsers: _SubParsersAction) -> None:
+    ...
+
+
+def main(args: Namespace) -> None:
     ...
