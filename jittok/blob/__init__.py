5b410ce60c5c97bea33c4715c1ca47b7a0e16dea
osoken <osoken.devel@outlook.jp>
2023-03-07 23:48:43 +0900

    feat(blob): add save_resource_from_http_request_in_temporary_file

--- jittok/blob/__init__.py
+++ jittok/blob/__init__.py
@@ -1,3 +1,3 @@
-from .core import open_zipfile
+from .core import open_zipfile, save_resource_from_http_request_in_temporary_file
 
-__all__ = ["open_zipfile"]
+__all__ = ["open_zipfile", "save_resource_from_http_request_in_temporary_file"]
